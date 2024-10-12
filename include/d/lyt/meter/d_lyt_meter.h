c921feaf226c91f47c29b00a0bf45bce6ab86260
robojumper <robojumper@gmail.com>
2024-10-12 13:36:20 +0200

    progress

--- include/d/lyt/meter/d_lyt_meter.h
+++ include/d/lyt/meter/d_lyt_meter.h
@@ -9,9 +9,16 @@
 #include <d/lyt/meter/d_lyt_meter_ganbari_gauge.h>
 #include <d/lyt/meter/d_lyt_meter_item_select.h>
 #include <d/lyt/meter/d_lyt_meter_minus_btn.h>
+
+#include <d/lyt/meter/d_lyt_meter_cross_btn.h>
+#include <d/lyt/meter/d_lyt_meter_plus_btn.h>
+#include <d/lyt/meter/d_lyt_meter_dowsing.h>
+#include <d/lyt/meter/d_lyt_meter_a_btn.h>
 // clang-format on
 
 class dLytMeter1Button_c : dLytMeterBase {
+    dLytMeter1Button_c() : mStateMgr(*this, sStateID::null) {}
+    virtual ~dLytMeter1Button_c() {}
 private:
     STATE_FUNC_DECLARE(dLytMeter1Button_c, Wait);
     STATE_FUNC_DECLARE(dLytMeter1Button_c, ToUse);
@@ -19,9 +26,13 @@ private:
     STATE_FUNC_DECLARE(dLytMeter1Button_c, Unuse);
 
     UI_STATE_MGR_DECLARE(dLytMeter1Button_c);
+    dLytSub mLyt;
+    d2d::AnmGroup_c mAnmGroups[3];
 };
 
 class dLytMeter2Button_c : dLytMeterBase {
+    dLytMeter2Button_c() : mStateMgr(*this, sStateID::null) {}
+    virtual ~dLytMeter2Button_c() {}
 private:
     STATE_FUNC_DECLARE(dLytMeter2Button_c, Wait);
     STATE_FUNC_DECLARE(dLytMeter2Button_c, ToUse);
@@ -29,6 +40,8 @@ private:
     STATE_FUNC_DECLARE(dLytMeter2Button_c, Unuse);
 
     UI_STATE_MGR_DECLARE(dLytMeter2Button_c);
+    dLytSub mLyt;
+    d2d::AnmGroup_c mAnmGroups[3];
 };
 
 class dLytMeterParts_c {
@@ -57,8 +70,12 @@ public:
     dLytMeterGanbariGauge_c mGanbariGauge;
     dLytMeterRupy_c mRupy;
     dLytMeterItemSelect_c mItemSelect;
-    dLytMeterRemoconBg_c mRemoCon;
+    dLytMeterCrossBtn_c mCrossBtn;
+    dLytMeterPlusBtn_c mPlusBtn;
     dLytMeterMinusBtn_c mMinusBtn;
+    dLytMeterDowsing_c mDowsing;
+    dLytMeterABtn_c mABtn;
+    dLytMeterRemoconBg_c mRemoCon;
     LytMeterGroup mMeters;
 
     u8 field_0x00[0x11E64];
@@ -73,6 +90,20 @@ public:
     /* 0x1373C */ mVec3_c mPos2;
     /* 0x13748 */ u8 field_0x13748[0x137B4 - 0x13748];
     /* 0x137B4 */ mVec3_c mPos3;
+};
+
+class dLytMeterContainer_c {
+public:
+    dLytMeterContainer_c();
+    virtual ~dLytMeterContainer_c() {
+        sInstance = nullptr;
+    }
+
+    static dLytMeterContainer_c *sInstance;
+private:
+    d2d::ResAccIf_c mResAcc;
+    u32 field_0x374;
+    dLytMeter_c mMeter;
 };
 
 #endif
